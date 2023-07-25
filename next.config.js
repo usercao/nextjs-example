1b4352720a4ba1604f21874bb9be928ee190e14e
usercao <cao858646882@qq.com>
2023-07-25 17:14:55 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en', 'sr', 'es', 'pseudo'],
+    defaultLocale: 'en',
+  },
+  experimental: {
+    swcPlugins: [['@lingui/swc-plugin', {}]],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css';
+// import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+import { i18n } from '@lingui/core';
+import { I18nProvider } from '@lingui/react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useLinguiInit } from '../utils';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useLinguiInit(pageProps.translation);
+
+  return (
+    <>
+      <I18nProvider i18n={i18n}>
+        <Component {...pageProps} />
+      </I18nProvider>
+    </>
+  );
 }
+
+export default MyApp;
