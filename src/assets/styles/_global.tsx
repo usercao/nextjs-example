ca45880d1d8e0517d384cfa4176f3587747fecd8
usercao <cao858646882@qq.com>
2023-08-01 11:17:51 +0800

    fix

--- src/assets/styles/_global.tsx
+++ src/assets/styles/_global.tsx
@@ -1,8 +1,67 @@
 import { createGlobalStyle } from 'styled-components';
 
+/**
+ * @abstract fix 无法自动格式化
+ * @link https://github.com/styled-components/vscode-styled-components/issues/175
+ */
 const styled = { createGlobalStyle };
 
-// fix 无法自动格式化 https://github.com/styled-components/vscode-styled-components/issues/175
+/**
+ * @link https://meyerweb.com/eric/tools/css/reset/
+ * @version v5.0.1 | 20191019
+ * @license License: none (public domain)
+ */
+// prettier-ignore
+const Reset = styled.createGlobalStyle`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, menu, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  main, menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, main, menu, nav, section {
+    display: block;
+  }
+  /* HTML5 hidden-attribute fix for newer browsers */
+  *[hidden] {
+    display: none;
+  }
+  body {
+    line-height: 1;
+  }
+  menu, ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
+
 export const GlobalStyle = styled.createGlobalStyle`
   /* fix 浏览器默认样式 */
   a,
